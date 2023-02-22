0583c6372888a2ee8bff6c5808504e60c084de45
Dev Pratap Singh <devthakur31931@gmail.com>
2023-02-22 20:00:34 +0530

    some changes made

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,21 +6,60 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
+
+async function getBalances(address){
+  const temp = await hre.ethers.provider.getBalance(address);
+  const bal = hre.ethers.utils.formatEther(temp);
+  return bal;
+}
+
+async function consoleBalance(addresses){
+  let count = 0;
+  for(const address of addresses){
+    console.log(`Address ${count} balance :`,await getBalances(address));
+    count++;
+  }
+}
+
+async function consoleMemos(memos){
+  for(const memo of memos){
+    const timestamp = memo.timestamp;
+    const name = memo.name;
+    const from = memo.from;
+    const message = memo.message;
+    console.log(`At ${timestamp},name ${name} , address ${from}, message ${message}`);
+  }
+}
+
+
+
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const [owner, from1 ,from2 ,from3] = await hre.ethers.getSigners();
+  const ins = await hre.ethers.getContractFactory("demo");
+  const contract = await ins.deploy();
+  await contract.deployed();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  console.log("Contract adress is : ",contract.address);
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const addresses = [owner.address,from1.address,from2.address,from3.address];
 
-  await lock.deployed();
+  console.log("Balances before buying cofee");
 
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  await consoleBalance(addresses);
+
+  const amount = {value : hre.ethers.utils.parseEther("1")};
+  await contract.connect(from1).buyCofee("from 1","nice job",amount);
+  await contract.connect(from2).buyCofee("from 2","good intiative",amount);
+  await contract.connect(from3).buyCofee("from 3","hello folk",amount);
+
+  console.log("Balances after buying cofee");
+
+  await consoleBalance(addresses);
+
+  const memos = await contract.getMemos();
+  consoleMemos(memos);
+  
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
